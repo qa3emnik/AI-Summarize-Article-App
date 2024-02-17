f6f708256c0aebeb19ad9e743bff5a466c6f316b
Qasem Nik <codewithqa3em@gmail.com>
2024-02-17 12:29:31 +0330

    update github link

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
         <button
           type='button'
           onClick={() =>
-            window.open("https://github.com/TidbitsJS/Summize", "_blank")
+            window.open("https://github.com/qa3emnik/AI-Summarize-Article-App", "_blank")
           }
           className='black_btn'
         >
